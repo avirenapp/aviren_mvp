b2059c995772570b15449bf3b5256da0d65873d2
Anders <48215209+Anders35@users.noreply.github.com>
2024-01-18 19:21:57 +0700

    Update chatbot.py

--- backend/chatbot.py
+++ backend/chatbot.py
@@ -4,13 +4,13 @@ from flask import Flask, request, jsonify
 import requests
 from google.cloud import firestore
 
-cred_path = 'REDACTED'
+cred_path = 'service.json'
 db = firestore.Client.from_service_account_json(cred_path)
 collection_name = 'users'
 
 app = Flask(__name__)
 
-genai.configure(api_key="REDACTED")
+genai.configure(api_key="AIzaSyC82X8ZxqCzVRJNYJJUC7M31UiVo3ew0-w")
 
 generation_config = {
     "temperature": 0.9,
@@ -26,17 +26,18 @@ safety_settings = [
     {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
 ]
 
-model = genai.GenerativeModel(model_name="gemini-pro",
-                              generation_config=generation_config,
-                              safety_settings=safety_settings)
-
-data = pickle.load(open("data.pkl", "rb"))
+model = genai.GenerativeModel(
+    model_name="gemini-pro",
+    generation_config=generation_config,
+    safety_settings=safety_settings,
+)
 
 prompt = "You are a helpful and joyous mental therapy assistant named Aviren. Always answer as helpfully and cheerfully as possible, while being safe. Please ensure that you answer the questions as humanly as possible as if you are having a conversation.\n\nIf a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.\n\n"
 last_prompt = "\n\nAnswer me in one paragraph. Do not use bullet points or listing your answer, make your response is not too long."
 
 @app.route('/generate', methods=['POST'])
 def generate_content():
+    data = pickle.load(open("data.pkl", "rb"))
     history = request.json
 
     if "email" not in history or "question" not in history:
@@ -93,7 +94,7 @@ def generate_content():
         return generated_text
     except Exception as e:
         print(e)
-        responseError = requests.post("REDACTED", headers={"Content-Type": "application/json"})
+        responseError = requests.post("https://paraphrase.pythonanywhere.com/generate", headers={"Content-Type": "application/json"})
         responseError.raise_for_status()
         generated_error = responseError.text
 
